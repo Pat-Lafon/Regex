6cfc28eaa15feff770e4dc65405eb33e8e343e18
Patrick LaFontaine <pwl45@cornell.edu>
2018-11-25 22:52:05 -0500

    updated mli with more functions

--- parser.mli
+++ parser.mli
@@ -5,6 +5,8 @@ type regexp
 (* Mainly for debugging, will be removed from mli at some point *)
 val regexp_to_string : regexp -> string
 
+
+(* Regular expressions *)
 val regexp : string -> regexp
 
 val regexp_case_fold : string -> regexp
@@ -14,3 +16,39 @@ val regexp_string : string -> regexp
 val regexp_string_case_fold : string -> regexp
 
 val string_match : regexp -> string -> int -> bool
+
+val string_forward : regexp -> string -> int -> int
+
+val string_backward : regexp -> string -> int -> int
+
+val string_partial_match : regexp -> string -> int -> bool 
+
+
+(* String replacement *)
+val global_replace : regexp -> string -> string -> string
+
+val replace_first : regexp -> string -> string -> string
+
+val global_substitute : regexp -> (string -> string) -> string -> string
+
+val substitute_first : regexp -> (string -> string) -> string -> string
+
+
+(* Splitting strings *)
+val split : regexp -> string -> string list
+
+val bounded_split : regexp -> string -> int -> string list
+
+val split_delim : regexp -> string -> string list
+
+val bounded_split_delim : regexp -> string -> int -> string list
+
+
+(* Extracting strings *)
+val string_before : string -> int -> string
+
+val string_after : string -> int -> string
+
+val first_chars : string -> int -> string
+
+val last_chars : string -> int -> string
